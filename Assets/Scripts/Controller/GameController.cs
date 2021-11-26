8cef34bbb684345203a81082baef1a1ce6763cf6
QQQ256 <78251233+QQQ256@users.noreply.github.com>
2021-11-25 22:51:15 -0500

    add timeUI

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -32,7 +32,7 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        TimeController.instance.BeginTimer();
     }
 
     // Update is called once per frame
